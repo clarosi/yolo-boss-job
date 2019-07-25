337568e744a06ec6de7b4fee269095e2fa7e7983
Ian R. Claros <clarosian@yahoo.com>
2019-07-26 06:37:12 +0800

    UI minor fixes

--- src/shared/components/Header/index.js
+++ src/shared/components/Header/index.js
@@ -5,6 +5,8 @@ import './Header.css';
 
 export const Header = () => (
   <header className="header">
-    <img src={Logo} width="120" alt="Bossjob" />
+    <a href="/">
+      <img src={Logo} width="120" alt="Bossjob" />
+    </a>
   </header>
 );
--- src/shared/components/Icon/index.js
+++ src/shared/components/Icon/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 
+import './Icon.css';
+
 export const Icon = props => {
   const { color = '#2379ea', children, isPadded, className } = props;
   let margin = '0 7px';
-  if (isPadded) margin = '4px 15px 0 0';
+  if (isPadded) margin = '3px 15px 0 0';
 
   return (
-    <i style={{ color, margin }} className={className} aria-hidden="true">
+    <i
+      style={{ color, margin }}
+      className={`${className} fa-icon`}
+      aria-hidden="true"
+    >
       {children}
     </i>
   );
